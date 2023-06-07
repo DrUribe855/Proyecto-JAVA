564e8f5609670e8d8e0fc8fc2b8bbf538d7f7649
Yeison Martinez <yeisonl2036@gmail.com>
2023-06-07 17:19:18 -0500

    Add: Modificar producto. Nota: Consultar evento btn

--- 21-FacturadorCompleto/src/Clases/DataBase.java
+++ 21-FacturadorCompleto/src/Clases/DataBase.java
@@ -200,4 +200,30 @@ public class DataBase {
         }
     }
     
+    public boolean modificarProducto(int id, String nombre, int precio){
+        try {
+            String consulta = "UPDATE productos SET nombre = '"+nombre+"',precio = '"+precio+"' WHERE id = '"+id+"' ";
+            int resp_consulta = manipularDB.executeUpdate(consulta);
+            if(resp_consulta > 0){
+                return true;
+            }else{
+                return false;
+            }
+        } catch (SQLException e) {
+            System.out.println("--> Error update --> " + e.getMessage());
+            return false;
+        }
+    }
+    
+    public ResultSet buscarProducto(int id){
+        try {
+            ResultSet productos = this.manipularDB.executeQuery("SELECT * FROM productos WHERE id = '"+id+"'");
+            productos.next();
+            return productos;
+        } catch (SQLException e) {
+            System.out.print("Error en busqueda --> " + e.getMessage());
+            return null;
+        }
+    }
+    
 }
